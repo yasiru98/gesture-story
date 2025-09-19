c255195c047aad18a5fe35499765461f832c20cf
yasiru98 <yik4325@g.rit.edu>
2025-09-19 17:10:42 -0400

    remove unused imports and vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import DebugPose from './pages/DebugPose';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return <DebugPose />; 
 }
